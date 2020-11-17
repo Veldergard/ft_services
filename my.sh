f6721186c1da8971bb191bf85e12c235cb58cc55
Artur <veldergard@gmail.com>
2020-11-17 18:46:05 +0300

    nginx fix

--- my.sh
+++ my.sh
@@ -2,7 +2,9 @@ mkdir -p ~/goinfre/.docker
 [ -f ~/.docker ] || ln -s ~/goinfre/.docker ~/.docker
 mkdir -p ~/goinfre/.minikube
 [ -f ~/.minikube ] || ln -s ~/goinfre/.minikube ~/.minikube
-brew reinstall minikube
+# brew reinstall minikube
+
+# eval $(minikube -p minikube docker-env)
 
 # docker-machine create olaurine
 # eval $(docker-machine env olaurine)
--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
+minikube stop
+minikube delete
 minikube start
 eval $(minikube docker-env)
 
-# Dashboard
 minikube dashboard &
 
-# install MetalLB
 minikube addons enable metallb
-kubectl apply -f srcs/metallb-config.yaml
+kubectl apply -f srcs/k8s/metallb-config.yaml
+
+docker build srcs/nginx -t "nginx:k8s"
+kubectl apply -f srcs/k8s/nginx.yaml
