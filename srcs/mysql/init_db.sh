c49763f4aadb3ad285f1fbc4c46e1a5e6ef32c58
Veldergard <veldergard@gmail.com>
2020-12-14 15:06:41 +0300

    123

--- srcs/mysql/init_db.sh
+++ srcs/mysql/init_db.sh
@@ -5,7 +5,7 @@ done
 
 echo "DROP DATABASE test" | mysql -u root --skip-password
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'user'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 mysql wordpress -u root --skip-password < wordpress.sql
