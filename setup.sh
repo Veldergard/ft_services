6684a110a08134fc9e2b2ed8157b354e42ea83d0
Veldergard <veldergard@gmail.com>
2020-12-16 20:43:49 +0300

    321

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-services=("wordpress" "phpmyadmin" "mysql") # "nginx" "ftps" "influx" "grafana"
+services=("wordpress" "phpmyadmin" "mysql" "nginx" "ftps" "influx" "grafana")
 
 minikube stop
 minikube delete
@@ -6,7 +6,6 @@ minikube start --vm-driver=virtualbox --disk-size 8GB
 eval $(minikube docker-env)
 
 minikube addons enable metallb
-kubectl apply -f srcs/k8s/metallb-config.yaml
 
 for service in "${services[@]}"
 do
@@ -14,9 +13,4 @@ do
 done
 
 minikube dashboard &
-kubectl apply -f srcs/k8s/volumes.yaml
-
-for service in "${services[@]}"
-do
-    kubectl apply -f srcs/k8s/${service}.yaml
-done
+kubectl apply -k srcs/k8s
--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -4,8 +4,9 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-wp core download
+wp core download --path=/var/www/
 wp config create --dbhost=mysql-svc --dbname=wordpress --dbuser=mysql --dbpass=mysql --skip-check
+chmod -R 755 /var/www /var/lib/nginx /var/www/*
 
 php-fpm7
 mysql wordpress -hmysql-svc -umysql -pmysql -e';';
@@ -14,11 +15,11 @@ do
     sleep 3;
     mysql wordpress -hmysql-svc -umysql -pmysql -e';';
 done;
-
-wp core install --url=http://192.168.99.100:5050 --title="olaurine's k8s services" --admin_user="olaurine" --admin_password="olaurine" --admin_email="veldergard@gmail.com"
-wp user create editor editor@gmail.com --role=editor --user_pass=editor
-wp user create author author@gmail.com --role=author --user_pass=author
-wp user create Vladimir Vladimir@gmail.com --role=subscriber --user_pass=Vladimir
-wp user create Viktor Viktor@gmail.com --role=subscriber --user_pass=Viktor
-
+if ! wp core is-installed ; then
+    wp core install --url=http://192.168.99.100:5050 --title="olaurine's k8s services" --admin_user="olaurine" --admin_password="olaurine" --admin_email="veldergard@gmail.com"
+    wp user create Annie annie@gmail.com --role=editor --user_pass=annie
+    wp user create Babay bebs@gmail.com --role=author --user_pass=bebs
+    wp user create Vladimir vladimir@gmail.com --role=contributor --user_pass=vladimir
+    wp user create Viktor viktor@gmail.com --role=subscriber --user_pass=viktor
+fi
 nginx -g 'daemon off;'
